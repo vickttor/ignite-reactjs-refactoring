b7522afff0b089638fb2a69eef2799ebaf8ef4c7
Victor Silva <victor.silvaPC@hotmail.com>
2022-02-15 23:44:53 -0300

    refactor: content component separated of the rest of code to improve responsability
    Besides refatoring I add a amount of film viewer

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,46 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from "../components/MovieCard";
+
+interface ContentProps {
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>;
+  selectedGenre: {
+    title: string;
+  };
+}
+
+export function Content(props: ContentProps) {
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {props.selectedGenre.title}</span>
+        </span>
+        <span className="moviesAmount">
+          Filmes: <span>{props.movies.length}</span>
+        </span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {props.movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
