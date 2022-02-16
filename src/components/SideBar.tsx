a66f704eff3f269687ac8211160f236097588443
Victor Silva <victor.silvaPC@hotmail.com>
2022-02-15 23:41:26 -0300

    refactor: :tada: Creation of SideBar component with toggle button
    To refactor the code sidebar component is separated of another codes

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,54 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "../components/Button";
+import { AiOutlineMenu } from "react-icons/ai/index";
+
+interface sideBarProps {
+  genres: Array<{
+    id: number;
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
+    title: string;
+  }>;
+
+  stateSelectedGenreId: {
+    setSelectedGenreId: (id: number) => void;
+    selectedGenreId: number;
+  };
+}
+
+export function SideBar(props: sideBarProps) {
+  function handleClickButton(id: number) {
+    props.stateSelectedGenreId.setSelectedGenreId(id);
+  }
+
+  function toggleSideBar() {
+    document.querySelector(".sidebar")?.classList.toggle("active");
+  }
+
+  return (
+    <nav className="sidebar">
+      <div className="sidebarToggleButton">
+        <button onClick={toggleSideBar}>
+          <AiOutlineMenu />
+        </button>
+      </div>
+
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {props.genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => {
+              toggleSideBar();
+              handleClickButton(genre.id);
+            }}
+            selected={props.stateSelectedGenreId.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
