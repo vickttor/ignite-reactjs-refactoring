057c1870e8b0ee46f373a0c8ed7427bf4ce30aa6
Victor Silva <victor.silvaPC@hotmail.com>
2022-02-18 12:43:50 -0300

    feat: embeded link around movie card to be redirect to Movie page

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,36 +1,55 @@
-import { Star, Clock } from 'react-feather';
+import { Star, Clock } from "react-feather";
+import { Link } from "react-router-dom";
 
-import '../styles/movie-card.scss';
+import "../styles/movie-card.scss";
 
 interface MovieCardProps {
   title: string;
   poster: string;
   rating: string;
   runtime: string;
+  movie: {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Year: string;
+    Released: string;
+    Production: string;
+  };
 }
 
 export function MovieCard(props: MovieCardProps) {
   return (
-    <div className="movie-card">
-      <img
-        src={props.poster}
-        alt={props.title}
-      />
+    <Link to={`/movies/${props.title}`} state={{ movie: props.movie }}>
+      <div className="movie-card">
+        <img src={props.poster} alt={props.title} />
 
-      <div>
-        <div className="movie-info">
-          <span>{props.title}</span>
-          <div className="meta">
-            <div>
-              <Star /> {props.rating}
-            </div>
+        <div>
+          <div className="movie-info">
+            <span>{props.title}</span>
+            <div className="meta">
+              <div>
+                <Star /> {props.rating}
+              </div>
 
-            <div>
-              <Clock /> {props.runtime}
+              <div>
+                <Clock /> {props.runtime}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    </Link>
+  );
 }
